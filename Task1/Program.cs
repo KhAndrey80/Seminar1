24ee28023fa676ee18445988537858e80b3ee98e
Andrey <A.com>
2023-08-03 23:54:07 +0300

    1

---
+++ Task1/Program.cs
@@ -0,0 +1,6 @@
+﻿//Написать программу, выдающую квадрат введенного числа
+Console.WriteLine("Введите число");
+string input = Console.ReadLine(); // int imput = Convert.ToInt32(Console.ReadLine())
+int number = Convert.ToInt32(input);
+int result = number * number;
+Console.WriteLine("Наш результат: " + result);

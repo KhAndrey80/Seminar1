e251cc444e2874eb991a823f0840ae21f71070af
Andrey <A.com>
2023-08-04 19:36:43 +0300

    1

---
+++ Task1/Zadacha1/Program.cs
@@ -0,0 +1,14 @@
+﻿//принять 2 числа и проверить, является ли первое число квадратом второго. a=25 b=5 да
+System.Console.WriteLine("Введите первое число");
+int first = Convert.ToInt32(System.Console.ReadLine());
+System.Console.WriteLine("Введите второе число");
+int second = Convert.ToInt32(System.Console.ReadLine());
+
+if (first == second * second)
+{
+    System.Console.WriteLine("Да, является квадратом");
+}
+else
+{
+   System.Console.WriteLine("Нет, не является"); 
+}

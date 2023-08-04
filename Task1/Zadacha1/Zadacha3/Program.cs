a1db384785e2dde3cef7a28a34b3d30622716b60
Andrey <A.com>
2023-08-04 20:36:41 +0300

    4

--- Task1/Zadacha1/Zadacha3/Program.cs
+++ Task1/Zadacha1/Zadacha3/Program.cs
@@ -1,14 +1,15 @@
 ﻿// Программа принимает число N, выводит числа от -N до N
 // Вторая задача: принимает на вход трехзначное число и на выходе показывает последнюю цифру
-Console.WriteLine("Введите число");
+/*Console.WriteLine("Введите число");
 int number1 = Convert.ToInt32(System.Console.ReadLine());
 int number2 = - number1;
 while (number2 <= number1)
 {
 System.Console.Write(number2 +" ");
     number2++;
-}
-
-    
- 
+}*/
+ Console.WriteLine("Введите трехзначное число");
+ int number = Convert.ToInt32(System.Console.ReadLine());
+ int result = number % 10;
+ System.Console.WriteLine(result);
 

9f6a3a87e820b7e3c62be5c48bfebd6ffbc07228
Andrey <A.com>
2023-08-05 11:55:45 +0300

    hw3

--- Task1/HomeWork/Program.cs
+++ Task1/HomeWork/Program.cs
@@ -17,7 +17,7 @@ else
 
 //Задача 4: Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел.2,3,7->7
 
-System.Console.WriteLine("Введите первое число");
+/*System.Console.WriteLine("Введите первое число");
 int first = Convert.ToInt32(System.Console.ReadLine());
 System.Console.WriteLine("Введите второе число");
 int second = Convert.ToInt32(System.Console.ReadLine());
@@ -38,4 +38,25 @@ if (max > tretye)
 {
    System.Console.WriteLine("Наибольшее число-> " + max);
 }
-else System.Console.WriteLine("Наибольшее число-> " + tretye);
+else System.Console.WriteLine("Наибольшее число-> " + tretye);*/
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка). 
+
+System.Console.WriteLine("Введите число");
+int number = Convert.ToInt32(System.Console.ReadLine());
+
+
+if ( number %2  == 0)
+{
+    System.Console.WriteLine("Число чётное");
+}
+else
+{
+  System.Console.WriteLine("Число нечётное");
+}
+
+
+
+
+
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.

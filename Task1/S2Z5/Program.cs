388f5c0a481278d54e54fce90193eb913c89569b
Andrey <A.com>
2023-08-06 21:02:27 +0300

    S2Z2

---
+++ Task1/S2Z5/Program.cs
@@ -0,0 +1,11 @@
+﻿//Выводит случайное трехзначное число удаляет вторую цифру
+
+Random rand = new Random();
+int number = rand.Next(100, 1000);
+
+System.Console.WriteLine(number);
+int a = number / 100;
+int b = number % 10; 
+int c = a*10 +b;
+System.Console.WriteLine(c);
+

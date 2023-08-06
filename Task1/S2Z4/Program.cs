0bb77a346a74a2f9c3b7cb0cef19329f93150628
Andrey <A.com>
2023-08-06 20:42:20 +0300

    S2Z1

---
+++ Task1/S2Z4/Program.cs
@@ -0,0 +1,20 @@
+﻿//Вывести случайное число из 10 до 99 и показать наибольшую цифру числа.
+Random rand = new Random();
+int number = rand.Next(10, 100); //генерирует случайные числа [ - входит число в диапазон, ( - не входит
+//int a = new Random().Next(10, 100) - альтернативный вариант записи
+
+int a = number / 10;
+int b = number % 10; 
+
+System.Console.WriteLine(number);
+if (a > b)
+{
+    System.Console.WriteLine("Максимальная цифра числa = " + a);
+}
+else if(b > a)
+{
+System.Console.WriteLine("Максимальная цифра числa = " + b);
+}
+else {
+System.Console.WriteLine($"Все цифры равны {a} = {b}");
+}

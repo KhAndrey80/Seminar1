68b8c8405e51f22c78099569bdc8f431b6250c83
Andrey <A.com>
2023-08-07 11:55:59 +0300

    S2Z5

--- Task1/S2Z5/S2Z6-7/Program.cs
+++ Task1/S2Z5/S2Z6-7/Program.cs
@@ -1,5 +1,5 @@
 ﻿// принимает 2 числа, проверяет, является ли первое кратным второму. Есл не кратно, выводить остаток от деления.
-System.Console.WriteLine("Введите первое число");
+/*System.Console.WriteLine("Введите первое число");
 int number1 = Convert.ToInt32(Console.ReadLine());
 
 System.Console.WriteLine("Введите второе число");
@@ -13,19 +13,16 @@ if (ost == 0) // || - или && - и
 else 
 {
     System.Console.WriteLine("Не кратно. Остаток: " + ost);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
+}*/
 
 //Принимает число и проверяет, кратно ли оно одновременно 7 и 23.
+System.Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+if (number1 %7 == 0 && number1 %23 == 0 ) // || - или && - и !- не
+{
+    System.Console.WriteLine("Кратно 7 и 23");
+}
+else 
+{
+    System.Console.WriteLine("Не кратно");
+}
